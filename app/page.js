5383629c540b5d87441948ae5505399689bd354f
Aldea-Andrei <aldeaandrei1999@yahoo.com>
2025-08-27 14:10:42 +0300

    Improvements

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Collapsible, CollapsibleContent } from "@/components/ui/collapsible";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { CollapsibleTrigger } from "@radix-ui/react-collapsible";
 import { useEffect, useRef, useState } from "react";
 import { JSONTree } from "react-json-tree";
 import {
@@ -23,6 +32,30 @@ export default function App() {
   const [timeSeries, setTimeSeries] = useState({});
   const intervalRef = useRef(null);
   const [interval, setIntervalValue] = useState(1000);
+  const [isCopied, setIsCopied] = useState({});
+  const [isFetching, setIsFetching] = useState(false);
+
+  const handleStart = () => {
+    setIsFetching(true);
+    startFetching();
+  };
+
+  const handleStop = () => {
+    setIsFetching(false);
+    stopFetching();
+  };
+
+  const handleCopy = (path, entries) => {
+    const text = JSON.stringify([...entries].reverse(), null, 2);
+
+    navigator.clipboard.writeText(text).then(() => {
+      setIsCopied((prev) => ({ ...prev, [path]: true }));
+
+      setTimeout(() => {
+        setIsCopied((prev) => ({ ...prev, [path]: false }));
+      }, 1000);
+    });
+  };
 
   // Load from localStorage
   useEffect(() => {
@@ -37,6 +70,10 @@ export default function App() {
   useEffect(() => {
     localStorage.setItem("selectedItems", JSON.stringify([...selected]));
   }, [selected]);
+
+  useEffect(() => {
+    fetchData();
+  }, [url]);
 
   const fetchData = async () => {
     try {
@@ -54,7 +91,7 @@ export default function App() {
           json
         );
 
-        if (value !== undefined && typeof value === "number") {
+        if (value !== undefined) {
           if (!stored[path]) stored[path] = [];
           stored[path].push({
             timestamp: new Date().toLocaleTimeString(),
@@ -130,7 +167,6 @@ export default function App() {
     base0F: "#ffffff",
   };
 
-  // Inside your component
   const deletePath = (path) => {
     // Remove from timeSeries
     const updatedSeries = { ...timeSeries };
@@ -150,17 +186,17 @@ export default function App() {
   return (
     <div className="p-5">
       <div className="w-full flex gap-2 justify-start items-center pb-4">
-        <label className="text-2xl font-bold">URL</label>
+        <label className="text-2xl font-bold  flex-1">URL</label>
         <Input
-          className="w-1/3"
+          className="flex-[3]"
           value={url}
           onChange={(e) => setUrl(e.target.value)}
         />
       </div>
       <div className="w-full flex gap-2 justify-start items-center pb-4">
-        <label className="text-lg font-bold">Interval (ms)</label>
+        <label className="text-lg font-bold flex-1">Interval (ms)</label>
         <Input
-          className="w-32"
+          className="w-32 flex-[3]"
           type="number"
           value={interval ?? ""}
           onChange={(e) =>
@@ -169,70 +205,117 @@ export default function App() {
             )
           }
         />
-        <Button onClick={startFetching}>Start fetch</Button>
-        <Button variant="destructive" onClick={stopFetching}>
-          Stop fetch
-        </Button>
       </div>
-      <div className="w-full flex gap-2 justify-start items-center pb-4">
+      <div className="flex gap-2">
+        {isFetching ? (
+          <Button variant="destructive" onClick={handleStop}>
+            Stop fetch
+          </Button>
+        ) : (
+          <Button onClick={handleStart}>Start fetch</Button>
+        )}
+      </div>
+      <div className="w-full flex gap-2 justify-start items-center mb-10">
         {data ? (
-          <JSONTree
-            data={data}
-            hideRoot={false}
-            valueRenderer={valueRenderer}
-            theme={myTheme}
-            invertTheme={false}
-          />
+          <div className="w-full min-h-[350px] overflow-auto">
+            <JSONTree
+              data={data}
+              hideRoot={false}
+              valueRenderer={valueRenderer}
+              theme={myTheme}
+              invertTheme={false}
+            />
+          </div>
         ) : (
           <p>No data loaded yet. Click "Start fetch".</p>
         )}
       </div>
-      <div className="w-full flex gap-2 justify-start items-center pb-4">
+      <div className="w-full flex-col gap-2 justify-start items-center pb-4">
         {Object.keys(timeSeries).length === 0 ? (
           <p>No values tracked yet.</p>
         ) : (
-          Object.entries(timeSeries).map(([path, entries]) => (
-            <div key={path} className="mb-6 flex flex-col">
-              <div className="flex items-center gap-2">
-                <strong>{path}</strong>
-                <Button
-                  variant="destructive"
-                  size="sm"
-                  onClick={() => deletePath(path)}
-                >
-                  Delete
-                </Button>
-              </div>
-              <ResponsiveContainer width="100%" height={200}>
-                <LineChart
-                  data={entries}
-                  margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-                >
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="timestamp" />
-                  <YAxis />
-                  <Tooltip />
-                  <Legend />
-                  <Line
-                    type="monotone"
-                    dataKey="value"
-                    stroke="#82ca9d"
-                    dot={false}
-                    isAnimationActive={false}
-                  />
-                </LineChart>
-              </ResponsiveContainer>
-              <ScrollArea className="mt-5 py-2 px-5 w-[500px] h-60 bg-accent border-black border rounded-lg shadow shadow-black">
-                <ul className="ml-4 mt-1 list-disc">
-                  {[...entries].reverse().map((entry, idx) => (
-                    <li key={idx}>
-                      {entry.timestamp} → {String(entry.value)}
-                    </li>
-                  ))}
-                </ul>
-              </ScrollArea>
-            </div>
-          ))
+          Object.entries(timeSeries).map(([path, entries]) => {
+            const numericEntries = entries.filter(
+              (entry) => typeof entry.value === "number" && !isNaN(entry.value)
+            );
+
+            return (
+              <Collapsible key={path}>
+                <Card key={path} className="my-5">
+                  <CardHeader>
+                    <CollapsibleTrigger>
+                      <CardTitle className="flex items-center gap-5">
+                        <Button
+                          variant="destructive"
+                          size="sm"
+                          onClick={() => deletePath(path)}
+                        >
+                          Delete
+                        </Button>
+                        <strong>{path}</strong>
+                        <div>Records: {entries.length} </div>
+                      </CardTitle>
+                    </CollapsibleTrigger>
+                  </CardHeader>
+                  <CollapsibleContent>
+                    <CardContent>
+                      <div className="mb-6 flex flex-col">
+                        <div className="w-full flex justify-start">
+                          <div className="flex-1">
+                            <Button
+                              className={`w-24 ${
+                                isCopied[path] ? "bg-green-600" : ""
+                              }`}
+                              onClick={() => handleCopy(path, entries)}
+                            >
+                              {isCopied[path] ? "Copied!" : "Copy values"}
+                            </Button>
+                            <ScrollArea className="mt-5 py-2 px-5 w-[500px] h-60 bg-accent border-black border rounded-lg shadow shadow-black">
+                              <ul className="ml-4 mt-1 list-disc">
+                                {[...entries].reverse().map((entry, idx) => (
+                                  <li key={idx}>
+                                    {entry.timestamp} → {String(entry.value)}
+                                  </li>
+                                ))}
+                              </ul>
+                            </ScrollArea>
+                          </div>
+                          <div className="flex-1">
+                            {numericEntries.length > 0 && (
+                              <ResponsiveContainer width="100%" height={300}>
+                                <LineChart
+                                  data={entries}
+                                  margin={{
+                                    top: 5,
+                                    right: 30,
+                                    left: 20,
+                                    bottom: 5,
+                                  }}
+                                >
+                                  <CartesianGrid strokeDasharray="3 3" />
+                                  <XAxis dataKey="timestamp" />
+                                  <YAxis />
+                                  <Tooltip />
+                                  <Legend />
+                                  <Line
+                                    type="monotone"
+                                    dataKey="value"
+                                    stroke="#82ca9d"
+                                    dot={false}
+                                    isAnimationActive={false}
+                                  />
+                                </LineChart>
+                              </ResponsiveContainer>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </CollapsibleContent>
+                </Card>
+              </Collapsible>
+            );
+          })
         )}
       </div>
     </div>
